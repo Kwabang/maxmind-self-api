fcf91ba0ffbf6b6c93790b715436d68722321e3b
Kwabang <kwabang2827@gmail.com>
2021-01-02 18:56:32 +0900

    Checking the existence of information

--- src/query.js
+++ src/query.js
@@ -1,68 +1,90 @@
 const maxmind = require('maxmind')
 
 const asn = (query_ip) => {
-	return new Promise((resolve, reject) => {
-		maxmind.open('./database/GeoLite2-ASN.mmdb').then((lookup) => {
-			resolve(lookup.get(query_ip))
-		}).catch((err) => {
+	return new Promise(async (resolve, reject) => {
+		let asn_data
+		try {
+			asn_data = (await maxmind.open('./database/GeoLite2-ASN.mmdb')).get(query_ip)
+		} catch (err) {
 			reject(err)
-		})
+		}
+		if (asn_data === null) {
+			reject("Maxmind doesn't have any information for that IP.")
+			return
+		}
+		resolve(asn_data)
 	})
 }
 
 const country = (query_ip) => {
-	return new Promise((resolve, reject) => {
-		maxmind.open('./database/GeoLite2-Country.mmdb').then((lookup) => {
-			resolve(lookup.get(query_ip))
-		}).catch((err) => {
+	return new Promise(async (resolve, reject) => {
+		let country_data
+		try {
+			country_data = (await maxmind.open('./database/GeoLite2-Country.mmdb')).get(query_ip)
+		} catch (err) {
 			reject(err)
-		})
+		}
+		if (country_data === null) {
+			reject("Maxmind doesn't have any information for that IP.")
+			return
+		}
+		resolve(country_data)
 	})
 }
 
 const city = (query_ip) => {
-	return new Promise((resolve, reject) => {
-		maxmind.open('./database/GeoLite2-City.mmdb').then((lookup) => {
-			resolve(lookup.get(query_ip))
-		}).catch((err) => {
+	return new Promise(async (resolve, reject) => {
+		let city_data
+		try {
+			city_data = (await maxmind.open('./database/GeoLite2-City.mmdb')).get(query_ip)
+		} catch (err) {
 			reject(err)
-		})
+		}
+		if (city_data === null) {
+			reject("Maxmind doesn't have any information for that IP.")
+			return
+		}
+		resolve(city_data)
 	})
 }
 
 const geolocation = (query_ip) => {
 	return new Promise(async (resolve, reject) => {
 		let asn_data
-		let city_asn
+		let city_data
 		try {
 			asn_data = (await maxmind.open('./database/GeoLite2-ASN.mmdb')).get(query_ip)
-			city_asn = (await maxmind.open('./database/GeoLite2-City.mmdb')).get(query_ip)
+			city_data = (await maxmind.open('./database/GeoLite2-City.mmdb')).get(query_ip)
 		} catch (err) {
 			reject(err)
 		}
+		if (asn_data === null || city_data === null) {
+			reject("Maxmind doesn't have any information for that IP.")
+			return
+		}
 		let response
-		if (city_asn.city) {
+		if (city_data.city) {
 			response = {
 				"ISP": asn_data.autonomous_system_organization,
-				"registered_country_code": city_asn.registered_country.iso_code,
-				"registered_country": city_asn.registered_country.names.en,
-				"city": city_asn.city.names.en,
-				"country_code": city_asn.country.iso_code,
-				"country": city_asn.country.names.en,
-				"continent_code": city_asn.continent.code,
-				"continent": city_asn.continent.names.en,
-				"location": city_asn.location
+				"registered_country_code": city_data.registered_country.iso_code,
+				"registered_country": city_data.registered_country.names.en,
+				"city": city_data.city.names.en,
+				"country_code": city_data.country.iso_code,
+				"country": city_data.country.names.en,
+				"continent_code": city_data.continent.code,
+				"continent": city_data.continent.names.en,
+				"location": city_data.location
 			}
 		} else {
 			response = {
 				"ISP": asn_data.autonomous_system_organization,
-				"registered_country_code": city_asn.registered_country.iso_code,
-				"registered_country": city_asn.registered_country.names.en,
-				"country_code": city_asn.country.iso_code,
-				"country": city_asn.country.names.en,
-				"continent_code": city_asn.continent.code,
-				"continent": city_asn.continent.names.en,
-				"location": city_asn.location
+				"registered_country_code": city_data.registered_country.iso_code,
+				"registered_country": city_data.registered_country.names.en,
+				"country_code": city_data.country.iso_code,
+				"country": city_data.country.names.en,
+				"continent_code": city_data.continent.code,
+				"continent": city_data.continent.names.en,
+				"location": city_data.location
 			}
 		}
 		resolve(response)
